271f773c4019ece317691af347f2c5f52daf9349
JamieH01 <Jacob__Henderson@outlook.com>
2023-05-01 14:34:54 -0400

    :P

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,83 @@
-use std::any::Any;
-use std::thread::{self, ScopedJoinHandle};
+#![warn(clippy::unwrap_used)]
+
+use std::marker::PhantomData;
+use std::thread;
 use std::time::{Duration, Instant};
 
-//trait AnySized: Any + ?Sized {}
+//use rand::prelude::*;
 
-//#[allow(dead_code)]
-pub struct BenchMarker<'scope, F, T>
-where
-    F: Fn() -> T + Sync + Send,
-    T: Any,
-{
-    pub code: F,
+#[macro_export] macro_rules! quickbench {
+    ($struct:ty, $threads:expr, $run:expr) => {
+        BenchMarker::<$struct>::new($threads, $run)
+    };
+    
+    ($name:ident, $struct:ty, $threads:expr, $run:expr) => {
+        let mut $name = BenchMarker::<$struct>::new($threads, $run);
+        $name.start();
+        println!(
+            "{:?} / {}",
+            $name.average(),
+            $name.max_threads * $name.max_runcount,
+        )
+    };
+}
+
+
+
+pub struct BenchMarker<T:Bench> {
+    phantom:PhantomData<T>,
     time_table: Vec<Duration>,
-    _thread_table: Vec<ScopedJoinHandle<'scope, Duration>>,
+    //_thread_table: Vec<ScopedJoinHandle<'scope, Duration>>,
     pub max_threads: usize,
     pub max_runcount: usize,
 }
 
-impl<'scope, F: Fn() -> T + Sync + Send, T: Any> BenchMarker<'_, F, T> {
-    pub fn new(code: F, max_threads: usize, max_runcount: usize) -> Self {
+impl<T:Bench> BenchMarker<T> {
+    pub fn new(max_threads: usize, max_runcount: usize) -> Self {
         Self {
-            code,
+            phantom:PhantomData,
             time_table: vec![],
-            _thread_table: vec![],
+            //_thread_table: vec![],
             max_threads,
             max_runcount,
         }
     }
 
-    fn _run_code(&mut self) {
-        let time = Instant::now();
-        (self.code)();
-        self.time_table.push(time.elapsed());
-    }
 
     pub fn start(&mut self) {
         thread::scope(|s| {
             for _ in 0..self.max_runcount {
-                for _ in 0..self.max_threads {
-                    let t = s.spawn(|| {
-                        let time = Instant::now();
-                        (self.code)();
-                        time.elapsed()
-                    });
-                    self.time_table.push(t.join().unwrap());
-                }
+            let mut scope_table = vec![];   
+            
+            for _ in 0..self.max_threads {
+                scope_table.push(s.spawn(|| {
+                    let mut item = <T as Bench>::generate();
+                    let time = Instant::now();
+                    item.test();
+                    time.elapsed()
+                }));
             }
-        });
+
+            for _ in 0..self.max_threads {
+                self.time_table.push(scope_table.pop().expect("Error joining scopes").join().expect("Error joining scopes"));
+            }
+        }
+    });
     }
 
     pub fn average(&self) -> Duration {
         let mut sum = Duration::ZERO;
-        self.time_table.iter().map(|i| sum += *i).collect::<()>();
+        self.time_table.iter().for_each(|i| sum += *i);
         sum / self.time_table.len() as u32
     }
+}
+
+
+
+
+
+
+pub trait Bench {
+    fn generate() -> Self;
+    fn test(&mut self);
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,15 +1,23 @@
 mod integration_tests {
+    //use std::{thread, time::Duration};
+
     use benchmark_suite::*;
 
     #[test]
     fn it_works() {
+        struct Table {}
 
-        let mut bench = BenchMarker::new(|| vec![15_usize; 1000000], 5, 20);
-        bench.start();
-        println!(
-            "{:?} / {}",
-            bench.average(),
-            bench.max_threads * bench.max_runcount
-        )
+        impl Bench for Table {
+            fn generate() -> Self {
+                Table {}
+            }
+
+            fn test(&mut self) {
+                let _dummy =vec![15; 10_000_000];
+            }
+        }
+
+        quickbench!(bench, Table, 8, 10);
+        
     }
 }
